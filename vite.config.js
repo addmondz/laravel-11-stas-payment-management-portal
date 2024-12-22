a9f99852771a55991a627e948cd5fc475655afb5
edmondlang <edmond_wuiming@hotmail.com>
2024-12-23 02:11:01 +0800

    yarn dev

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,7 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
-const baseUrl = process.env.VITE_BASE_URL_PUBLIC;
-console.log(process.env.VITE_BASE_URL_PUBLIC);
-
 export default defineConfig({
-    base: baseUrl,
     plugins: [
         laravel({
             input: 'resources/js/app.js',
@@ -21,9 +17,4 @@ export default defineConfig({
             },
         }),
     ],
-    server: {
-        proxy: {
-            '/@vite': baseUrl + '/build',
-        },
-    },
 });
