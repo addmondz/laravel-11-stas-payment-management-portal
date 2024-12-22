95ba75a0d5010b673e2ffe83c2646231e738ad64
edmondlang <edmond_wuiming@hotmail.com>
2024-12-23 02:38:06 +0800

    yarn run build

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,10 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
+const baseUrl = 'http://deeppink-mule-517335.hostingersite.com/public/build';
+
 export default defineConfig({
+    base: baseUrl,
     plugins: [
         laravel({
             input: 'resources/js/app.js',
@@ -17,4 +20,9 @@ export default defineConfig({
             },
         }),
     ],
+    server: {
+        proxy: {
+            '/@vite': baseUrl + '/build',
+        },
+    },
 });
