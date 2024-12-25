ceb1066b1f198c08af6fc0cefde5262f88197c77
edmondlang <edmond_wuiming@hotmail.com>
2024-12-25 12:42:29 +0800

    update

--- resources/js/Components/CreateClaimForm.vue
+++ resources/js/Components/CreateClaimForm.vue
@@ -8,8 +8,10 @@ import { useForm } from '@inertiajs/vue3';
 import { ref } from 'vue';
 import { onMounted } from 'vue';
 import Swal from 'sweetalert2';
+import LoadingComponent from '@/Components/LoadingComponent.vue';
 
 const showingCreateClaimModal = ref(false);
+const formIsLoading = ref(false);
 const emit = defineEmits();
 const currencyData = ref([]);
 const paymentCategoryData = ref([]);
@@ -34,6 +36,7 @@ const closeModal = () => {
 };
 
 const submitClaim = async () => {
+    formIsLoading.value = true;
     try {
         // Create FormData to handle file uploads
         const formData = new FormData();
@@ -65,6 +68,7 @@ const submitClaim = async () => {
         closeModal();
         form.reset();
         emit('createComplete', true);
+        formIsLoading.value = false;
     } catch (err) {
         // Capture the API error and update form.errors
         if (err.response && err.response.data.errors) {
@@ -78,6 +82,7 @@ const submitClaim = async () => {
                 confirmButtonText: 'OK',
             });
         }
+        formIsLoading.value = false;
     }
 };
 
@@ -112,100 +117,103 @@ onMounted(() => {
 
         <Modal :show="showingCreateClaimModal" @close="closeModal">
             <form @submit.prevent="submitClaim" class="p-6">
-                <h2 class="text-lg font-medium text-gray-900">Create a New Claim</h2>
+                <div v-if="formIsLoading">
+                    <LoadingComponent class="mt-32 mb-32 " />
+                </div>
+                <div v-else>
+                    <h2 class="text-lg font-medium text-gray-900">Create a New Claim</h2>
+                    <div class="grid grid-cols-1 gap-4 mt-4">
+                        <div>
+                            <InputLabel for="payment_type" value="Payment Type" />
+                            <select id="payment_type" v-model="form.payment_type"
+                                class="mt-1 block w-full border-gray-300 rounded-md shadow-sm" required>
+                                <option value="" disabled selected>Please select Payment Type</option>
+                                <option value="reimbursement">Reimbursement</option>
+                                <option value="external_payment">External Payment</option>
+                            </select>
+                            <InputError :message="form.errors.payment_type" class="mt-2" />
+                        </div>
 
-                <div class="grid grid-cols-1 gap-4 mt-4">
-                    <div>
-                        <InputLabel for="payment_type" value="Payment Type" />
-                        <select id="payment_type" v-model="form.payment_type"
-                            class="mt-1 block w-full border-gray-300 rounded-md shadow-sm" required>
-                            <option value="" disabled selected>Please select Payment Type</option>
-                            <option value="reimbursement">Reimbursement</option>
-                            <option value="external_payment">External Payment</option>
-                        </select>
-                        <InputError :message="form.errors.payment_type" class="mt-2" />
-                    </div>
+                        <div>
+                            <InputLabel for="payment_category" value="Payment Category" />
+                            <select id="currency" v-model="form.payment_category"
+                                class="mt-1 payment_category w-full border-gray-300 rounded-md shadow-sm" required>
+                                <option value="" disabled selected>Please select Payment Category</option>
+                                <option v-for="(name, code) in paymentCategoryData" :key="code" :value="code"
+                                    class="capitalize">
+                                    {{ name }}
+                                </option>
+                            </select>
+                            <InputError :message="form.errors.payment_category" class="mt-2" />
+                        </div>
 
-                    <div>
-                        <InputLabel for="payment_category" value="Payment Category" />
-                        <select id="currency" v-model="form.payment_category"
-                            class="mt-1 payment_category w-full border-gray-300 rounded-md shadow-sm" required>
-                            <option value="" disabled selected>Please select Payment Category</option>
-                            <option v-for="(name, code) in paymentCategoryData" :key="code" :value="code"
-                                class="capitalize">
-                                {{ name }}
-                            </option>
-                        </select>
-                        <InputError :message="form.errors.payment_category" class="mt-2" />
-                    </div>
+                        <div>
+                            <InputLabel for="currency" value="Currency" />
+                            <select id="currency" v-model="form.currency"
+                                class="mt-1 block w-full border-gray-300 rounded-md shadow-sm" required>
+                                <option value="" disabled selected>Please select Currency</option>
+                                <option v-for="(name, code) in currencyData" :key="code" :value="code">
+                                    {{ name }}
+                                </option>
+                            </select>
+                            <InputError :message="form.errors.currency" class="mt-2" />
+                        </div>
 
-                    <div>
-                        <InputLabel for="currency" value="Currency" />
-                        <select id="currency" v-model="form.currency"
-                            class="mt-1 block w-full border-gray-300 rounded-md shadow-sm" required>
-                            <option value="" disabled selected>Please select Currency</option>
-                            <option v-for="(name, code) in currencyData" :key="code" :value="code">
-                                {{ name }}
-                            </option>
-                        </select>
-                        <InputError :message="form.errors.currency" class="mt-2" />
-                    </div>
+                        <div>
+                            <InputLabel for="amount" value="Amount (incl. GST if any)" />
+                            <TextInput id="amount" v-model="form.amount" type="number" step="0.01"
+                                placeholder="Enter amount" class="mt-1 block w-full" required />
+                            <InputError :message="form.errors.amount" class="mt-2" />
+                        </div>
 
-                    <div>
-                        <InputLabel for="amount" value="Amount (incl. GST if any)" />
-                        <TextInput id="amount" v-model="form.amount" type="number" step="0.01"
-                            placeholder="Enter amount" class="mt-1 block w-full" required />
-                        <InputError :message="form.errors.amount" class="mt-2" />
-                    </div>
-
-                    <div>
-                        <div class="col-span-2">
-                            <InputLabel value="GST" />
-                            <div class="flex items-center space-x-4 mt-1">
-                                <label class="inline-flex items-center">
-                                    <input type="radio" v-model="form.gst" value="1" class="form-radio" />
-                                    <span class="ml-2">Yes</span>
-                                </label>
-                                <label class="inline-flex items-center">
-                                    <input type="radio" v-model="form.gst" value="0" class="form-radio" />
-                                    <span class="ml-2">No</span>
-                                </label>
+                        <div>
+                            <div class="col-span-2">
+                                <InputLabel value="GST" />
+                                <div class="flex items-center space-x-4 mt-1">
+                                    <label class="inline-flex items-center">
+                                        <input type="radio" v-model="form.gst" value="1" class="form-radio" />
+                                        <span class="ml-2">Yes</span>
+                                    </label>
+                                    <label class="inline-flex items-center">
+                                        <input type="radio" v-model="form.gst" value="0" class="form-radio" />
+                                        <span class="ml-2">No</span>
+                                    </label>
+                                </div>
+                                <InputError :message="form.errors.gst" class="mt-2" />
                             </div>
-                            <InputError :message="form.errors.gst" class="mt-2" />
                         </div>
-                    </div>
 
-                    <div>
-                        <div class="col-span-2">
-                            <InputLabel for="purpose" value="Purpose of Payment" />
-                            <textarea id="purpose" v-model="form.purpose" rows="4"
-                                placeholder="Enter purpose of payment"
-                                class="mt-1 block w-full border-gray-300 rounded-md shadow-sm" required></textarea>
-                            <InputError :message="form.errors.purpose" class="mt-2" />
+                        <div>
+                            <div class="col-span-2">
+                                <InputLabel for="purpose" value="Purpose of Payment" />
+                                <textarea id="purpose" v-model="form.purpose" rows="4"
+                                    placeholder="Enter purpose of payment"
+                                    class="mt-1 block w-full border-gray-300 rounded-md shadow-sm" required></textarea>
+                                <InputError :message="form.errors.purpose" class="mt-2" />
+                            </div>
                         </div>
-                    </div>
 
-                    <div>
-                        <InputLabel for="receipt_date" value="Date of Receipt / Invoice" />
-                        <TextInput id="receipt_date" v-model="form.receipt_date" type="date" class="mt-1 block w-full"
-                            required />
-                        <InputError :message="form.errors.receipt_date" class="mt-2" />
-                    </div>
-
-                    <div>
-                        <div class="col-span-2">
-                            <InputLabel for="receipt" value="Upload Receipt" />
-                            <input id="receipt" type="file" @change="(e) => (form.receipt = e.target.files[0])"
-                                accept=".jpeg,.jpg,.pdf,.png"
-                                class="mt-1 block w-full border border-gray-300 rounded-md shadow-sm p-2 cursor-pointer"
+                        <div>
+                            <InputLabel for="receipt_date" value="Date of Receipt / Invoice" />
+                            <TextInput id="receipt_date" v-model="form.receipt_date" type="date" class="mt-1 block w-full"
                                 required />
-                            <InputError :message="form.errors.receipt" class="mt-2" />
+                            <InputError :message="form.errors.receipt_date" class="mt-2" />
+                        </div>
+
+                        <div>
+                            <div class="col-span-2">
+                                <InputLabel for="receipt" value="Upload Receipt" />
+                                <input id="receipt" type="file" @change="(e) => (form.receipt = e.target.files[0])"
+                                    accept=".jpeg,.jpg,.pdf,.png"
+                                    class="mt-1 block w-full border border-gray-300 rounded-md shadow-sm p-2 cursor-pointer"
+                                    required />
+                                <InputError :message="form.errors.receipt" class="mt-2" />
+                            </div>
                         </div>
                     </div>
-
                 </div>
 
-                <div class="text-right mt-6">
+                <div class="text-right mt-6" v-if="!formIsLoading">
                     <button type="button" @click="closeModal"
                         class="bg-white hover:bg-gray-100 text-black inline-flex items-center px-4 py-2 border rounded-md font-semibold text-xs uppercase tracking-widest focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150">
                         Close
