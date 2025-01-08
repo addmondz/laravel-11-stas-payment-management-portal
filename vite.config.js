460662b967ac4bdf20f243eba3170f9629c464aa
edmondlang <edmond_wuiming@hotmail.com>
2025-01-08 14:25:26 +0800

    updates

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,10 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
-const baseUrl = 'http://deeppink-mule-517335.hostingersite.com/public/build';
+const baseUrl = 'http://deeppink-mule-517335.hostingersite.com/build'; // No /public here
 
 export default defineConfig({
-    base: baseUrl,
+    base: baseUrl, // This should point to the build folder
     plugins: [
         laravel({
             input: 'resources/js/app.js',
@@ -22,7 +22,7 @@ export default defineConfig({
     ],
     server: {
         proxy: {
-            '/@vite': baseUrl + '/build',
+            '/@vite': baseUrl, // Proxy the build folder correctly
         },
     },
 });
