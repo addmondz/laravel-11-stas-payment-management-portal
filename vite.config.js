9255e73f67f1115f18db2d59c9393c9746442fd7
edmondlang <edmond_wuiming@hotmail.com>
2025-01-08 14:29:26 +0800

    test

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,8 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
-const baseUrl = 'http://deeppink-mule-517335.hostingersite.com/build'; // No /public here
+// const baseUrl = 'http://deeppink-mule-517335.hostingersite.com/build'; // No /public here
+const baseUrl = 'https://staspay.at-eases.com/build'; // No /public here
 
 export default defineConfig({
     base: baseUrl, // This should point to the build folder
