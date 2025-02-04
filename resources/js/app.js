c04fdbb4b781b4f1340c72686afaf073540642ef
edmondlang <edmond_wuiming@hotmail.com>
2025-02-04 20:25:51 +0800

    update

--- resources/js/app.js
+++ resources/js/app.js
@@ -9,7 +9,7 @@ import { ZiggyVue } from '../../vendor/tightenco/ziggy';
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 createInertiaApp({
-    title: (title) => `${title} - ${appName}`,
+    title: (title) => `${title} - STAS Payment Management Portal`,
     resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
     setup({ el, App, props, plugin }) {
         const app = createApp({ render: () => h(App, props) })
